ba7af259a8078bdd0f63a4a3c060df071071a9a7
Rich Jones <rich@anomos.info>
2016-06-28 17:47:47 -0400

    blah blah

--- adgmaker.py
+++ adgmaker.py
@@ -19,13 +19,15 @@ class ADGMaker(object):
         Parses command, load settings and dispatches accordingly.
 
         """
-        help_message = "Please supply a command to execute."
-
+        help_message = "Please supply a path to a folder of MP3s."
         parser = argparse.ArgumentParser(description='ADGMaker - Create Ableton Live Instruments.\n')
-        # parser.add_argument('command_env', metavar='U', type=str, nargs='*', help=help_message)
-
+        parser.add_argument('samples_path', metavar='U', type=str, nargs='*', help=help_message)
         args = parser.parse_args(argv)
         vargs = vars(args)
+
+        if not vargs['samples_path']:
+            print(help_message)
+            return
 
     def add_mp3_to_instrument(file_path):
         """
@@ -45,7 +47,7 @@ class ADGMaker(object):
         """
         return
 
-    def write_adg(instrument_name):
+    def write_adg(adg_name):
         """
         Create the final ADG file.
         """
