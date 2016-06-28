b856bd90b3d2ae3f7f669510d74ce267551a60df
Rich Jones <rich@anomos.info>
2016-06-28 17:42:58 -0400

    more stub

--- adgmaker.py
+++ adgmaker.py
@@ -27,6 +27,29 @@ class ADGMaker(object):
         args = parser.parse_args(argv)
         vargs = vars(args)
 
+    def add_mp3_to_instrument(file_path):
+        """
+        Given a complete file path, add to the XML for this instrument.
+
+        Samples are in the format:
+            {{instrument_name}}_{{note}}_{{length}}_{{velocity}}_{{type}}.mp3
+        Ex:
+            double-bass_Gs3_1_piano_arco-normal.mp3
+
+        """
+        return
+        
+    def create_base_xml():
+        """
+        Create the standard cruft XML for the ADG.
+        """
+        return
+
+    def write_adg(instrument_name):
+        """
+        Create the final ADG file.
+        """
+        return
 
 def handle(): # pragma: no cover
     """
